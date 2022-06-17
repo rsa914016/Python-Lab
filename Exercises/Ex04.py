07b706f1ea0acf3ee00ea5244f7cb42e323d3384
rsa914016 <86813260+rsa914016@users.noreply.github.com>
2022-06-17 18:49:37 +0530

    Update Ex04.py

--- Exercises/Ex04.py
+++ Exercises/Ex04.py
@@ -1,10 +1,13 @@
 def Ex04a():
     file1 = input('Enter 1st File Name : ')
     file2 = input('Enter 2nd File Name : ')
+    text = input('Enter The Text To File1:')
+    f1 = open(f'../Text Files/{file1}', 'w')
+    f1.write(text)
+    f1.close()
     f1 = open(f'../Text Files/{file1}', 'r')
     f2 = open(f'../Text Files/{file2}', 'w')
-    for line in f1.readlines():
-        f2.write(line)
+    f2.write(f1.read())
     f1.close()
     f2.close()
     print('Text Copied SuccessFully')
@@ -38,6 +41,6 @@ class IntToRoman:
 
 
 if __name__ == '__main__':
-    # Ex04a()
-    Ex04b()
+    Ex04a()
+    # Ex04b()
     # IntToRoman().Convert(1995)
