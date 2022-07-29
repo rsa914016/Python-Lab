79d3ff068b4b993b9981f13a01e4fe22980cdc9c
rsa914016 <86813260+rsa914016@users.noreply.github.com>
2022-07-29 10:40:19 +0530

    Update test_13a.py

--- Exercises/test_13a.py
+++ Exercises/test_13a.py
@@ -1,25 +1,14 @@
 import unittest
-from Ex13 import isEven, isOdd
+from Ex13 import isEvenLst
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self):
-        print('SetUp')
-
-    def tearDown(self):
-        print('TearDown')
-
+   
     def test_isEven(self):
-        self.assertEqual(isEven(2), True)
-        self.assertEqual(isEven(65), False)
-        self.assertEqual(isEven(8), True)
-        self.assertEqual(isEven(100), True)
-
-    def test_isOdd(self):
-        self.assertEqual(isOdd(2), False)
-        self.assertEqual(isOdd(65), True)
-        self.assertEqual(isOdd(8), False)
-        self.assertEqual(isOdd(101), True)
+        self.assertEqual(isEvenLst([]), True)
+        self.assertEqual(isEvenLst([2, 4, 88, 99]), False)
+        self.assertEqual(isEvenLst([2, 8, 90, 10]), True)
+        self.assertEqual(isEvenLst([44, 66, 88, 22]), True)
 
 
 if __name__ == '__main__':
